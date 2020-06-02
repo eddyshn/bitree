a4f58094a9989426f393ab8dec2c6bdafa80ace2
eddyshn <eddy.shan@outlook.com>
2020-06-02 08:41:51 +0800

    mybatis configuration

--- vhr/server/src/main/java/com/bitree/server/ServerApplication.java
+++ vhr/server/src/main/java/com/bitree/server/ServerApplication.java
@@ -1,10 +1,11 @@
 package com.bitree.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MappingScan(basePackages = "")
+@MapperScan(basePackages = "com.bitree.server.mapper")
 public class ServerApplication {
 
     public static void main(String[] args) {
