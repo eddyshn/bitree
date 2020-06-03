daddf1a4f4487bea7eceb3a1129c9ad6754c6557
eddyshn <eddy.shan@outlook.com>
2020-06-03 08:24:17 +0800

    login-1

---
+++ vhr/server/src/main/java/com/bitree/server/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.bitree.server.config;
+
+import com.bitree.server.service.HRService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    HRService hrService;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth.userDetailsService(hrService);
+    }
+}
--- vhr/server/src/main/java/com/bitree/server/model/Hr.java
+++ vhr/server/src/main/java/com/bitree/server/model/Hr.java
@@ -76,17 +76,17 @@ public class Hr implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
