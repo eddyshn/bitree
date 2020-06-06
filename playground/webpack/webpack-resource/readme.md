b92a110a7bbb59315295f297a495c530be8292c1
eddy <eddy.shan@ribitwo.com>
2020-06-06 11:09:07 +0800

    webpack development

---
+++ playground/webpack/webpack-resource/readme.md
@@ -0,0 +1,1 @@
+[refer to](https://www.webpackjs.com/guides/asset-management/)
---
+++ playground/webpack/webpack-sourcemap/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app:'./src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+          {
+              title: 'Output Management'
+          }
+      ),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+};
