58a42033e416bb4b2ee33709b620ad194b164e6f
eddy <eddy.shan@ribitwo.com>
2020-07-01 08:08:55 +0800

    add readme

---
+++ demo/vue/03/index.js
@@ -0,0 +1,53 @@
+var app = new Vue(
+    { el: '#app',
+        data: {
+            question: '',
+            answer: 'I cannot give you an answer until you ask a question!',
+            name: '',
+            newId: 3,
+            list: [
+                { id: 1, name: '李斯' },
+                { id: 2, name: '吕不韦' },
+                { id: 3, name: '嬴政' }
+            ]
+        },
+        watch: {
+            // 如果 `question` 发生改变，这个函数就会运行
+            question: function (newQuestion, oldQuestion) {
+                this.answer = 'Waiting for you to stop typing...'
+                this.debouncedGetAnswer()
+            }
+        },
+        created: function () {
+            // `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
+            // 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
+            // AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
+            // `_.debounce` 函数 (及其近亲 `_.throttle`) 的知识，
+            // 请参考：https://lodash.com/docs#debounce
+            this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+        },
+        methods: {
+            getAnswer: function () {
+                if (this.question.indexOf('?') === -1) {
+                    this.answer = 'Questions usually contain a question mark. ;-)'
+                    return
+                }
+                this.answer = 'Thinking...'
+                var vm = this
+                axios.get('https://yesno.wtf/api')
+                    .then(function (response) {
+                        vm.answer = _.capitalize(response.data.answer)
+                    })
+                    .catch(function (error) {
+                        vm.answer = 'Error! Could not reach the API. ' + error
+                    })
+            },
+
+            add() {
+                //注意这里是unshift
+                this.list.unshift({ id: ++this.newId, name: this.name })
+                this.name = ''
+            }
+        }
+    }
+);
