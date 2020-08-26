3392e9097d7506167cd6273ab0b9b21d2ab17867
eddyshn <eddy.shan@outlook.com>
2020-08-26 22:29:54 +0800

    security

--- KB/java/spring-security/demo/01/src/main/java/com/eddy/security01/config/MultiSecurityConfig.java
+++ KB/java/spring-security/demo/01/src/main/java/com/eddy/security01/config/MultiSecurityConfig.java
@@ -7,10 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -26,20 +28,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true)
 public class MultiSecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //password get from Security01ApplicationTests
         auth.inMemoryAuthentication()
-                .withUser("eddy").password("$2a$10$qgkM16ida5xiY9muaKx7YOA5v4Fz4yCmRPFDvBhd7UlxcenNVcXT2").roles("admin")
+                .withUser("eddy3").password("$2a$10$G3kVAJHvmRrr6sOj.j4xpO2Dsxl5EG8rHycPHFWyi9UMIhtdSH15u").roles("admin")
                 .and()
-                .withUser("eddy2").password("$2a$10$zc2q4lV482NZeehGaOiRputlunyqWsQapJtZM1kIgDiMCn6XIWtIG").roles("user");
+                .withUser("eddy4").password("$2a$10$kWjG2GxWhm/2tN2ZBpi7bexXjUneIKFxIAaMYJzY7WcziZLCD4PZS").roles("user");
     }
 
     @Configuration
@@ -47,79 +49,22 @@ public class MultiSecurityConfig {
     public static class AdminSecurityConfig extends WebSecurityConfigurerAdapter{
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-                    .antMatchers("/admin/**").hasRole("admin");
+            http.antMatcher("/admin/**").authorizeRequests().anyRequest().hasAnyRole("admin");
         }
     }
 
-    public static class OtherSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Configuration
+    public static class OtherSecurityConfig extends WebSecurityConfigurerAdapter{
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-                    .anyRequest().authenticated()
+            http.authorizeRequests().anyRequest().authenticated()
                     .and()
                     .formLogin()
-                    .loginProcessingUrl("/doLogin")//点击登陆按钮时访问的URL,postman隥陆URL
+                    .loginProcessingUrl("/doLogin")
                     .loginPage("/login")//登陆页面
                     .usernameParameter("uname")
                     .passwordParameter("passwd")
-                    .successHandler(new AuthenticationSuccessHandler() {
-                        @Override
-                        public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                            httpServletResponse.setContentType("application/json;charset=utf-8");
-                            PrintWriter out = httpServletResponse.getWriter();
-                            Map<String, Object> map = new HashMap<>();
-                            map.put("status-other", 200);
-                            map.put("msg-other", authentication.getPrincipal());
-                            out.write(new ObjectMapper().writeValueAsString(map));
-                            out.flush();
-                            out.close();
-                        }
-                    })
-                    .failureHandler(new AuthenticationFailureHandler() {
-                        @Override
-                        public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-                            httpServletResponse.setContentType("application/json;charset=utf-8");
-                            PrintWriter out = httpServletResponse.getWriter();
-                            Map<String, Object> map = new HashMap<>();
-                            map.put("status-other", 401);
-
-                            if (e instanceof LockedException){
-                                map.put("msg-other", "账户锁定");
-                            } else if (e instanceof BadCredentialsException){
-                                map.put("msg-other", "用户名或密码错误");
-                            } else  if (e instanceof DisabledException){
-                                map.put("msg-other", "账户禁用");
-                            } else if (e instanceof AccountExpiredException){
-
-                            } else if (e instanceof CredentialsExpiredException){
-
-                            } else {
-                                map.put("msg-other", "登陆失败");
-                            }
-
-                            out.write(new ObjectMapper().writeValueAsString(map));
-                            out.flush();
-                            out.close();
-                        }
-                    })
                     .permitAll()
-                    .and()
-                    .logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessHandler(new LogoutSuccessHandler() {
-                        @Override
-                        public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                            httpServletResponse.setContentType("application/json;charset=utf-8");
-                            PrintWriter out = httpServletResponse.getWriter();
-                            Map<String, Object> map = new HashMap<>();
-                            map.put("status-other", 200);
-                            map.put("msg-other", "注销成功");
-                            out.write(new ObjectMapper().writeValueAsString(map));
-                            out.flush();
-                            out.close();
-                        }
-                    })
                     .and()
                     .csrf().disable();
         }
